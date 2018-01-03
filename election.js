daa783792e9d01dcfc348e4f641194d81699cc5e
Situ William <situ_william@yahoo.com>
2018-01-03 13:17:05 -0500

    displayed election results

--- election.js
+++ election.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-
   // Imagination!
-
+  $.ajax({
+  	url: 'https://bb-election-api.herokuapp.com/',
+  	method: 'GET',
+  	dataType: 'JSON'
+  }).done(function(response){
+  	var election = document.querySelector('#election')
+  	for (var i = 0; i < response.candidates.length; i++){
+  		candidate = `<li>Name: ${response.candidates[i].name}<br>Votes: ${response.candidates[i].votes}</li><br>`;
+  		election.insertAdjacentHTML('beforeend',candidate)
+  	}
+  })
 });
